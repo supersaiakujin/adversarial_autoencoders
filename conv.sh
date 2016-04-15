0573bd17b47a7c954bc1938c46988f2b16956f8d
supersaiakujin <maaasaaataaakaaaooohaaashiii@gmail.com>
2016-04-16 02:13:28 +0900

    added example

---
+++ conv.sh
@@ -0,0 +1,24 @@
+convert -delay 20 -loop 0 img/test_epoch0*.png q_z_f10.gif
+convert -delay 20 -loop 0 dec/genimg_e00*_d00.png genimg_d00.gif
+convert -delay 20 -loop 0 dec/genimg_e00*_d01.png genimg_d01.gif
+convert -delay 20 -loop 0 dec/genimg_e00*_d02.png genimg_d02.gif
+convert -delay 20 -loop 0 dec/genimg_e00*_d03.png genimg_d03.gif
+convert -delay 20 -loop 0 dec/genimg_e00*_d04.png genimg_d04.gif
+convert -delay 20 -loop 0 dec/genimg_e00*_d05.png genimg_d05.gif
+convert -delay 20 -loop 0 dec/genimg_e00*_d06.png genimg_d06.gif
+convert -delay 20 -loop 0 dec/genimg_e00*_d07.png genimg_d07.gif
+convert -delay 20 -loop 0 dec/genimg_e00*_d08.png genimg_d08.gif
+convert -delay 20 -loop 0 dec/genimg_e00*_d09.png genimg_d09.gif
+
+#convert -resize 160x -delay 20 -loop 0 img/test_epoch0*.png s_q_z_f10.gif
+#convert -resize 240x -delay 20 -loop 0 dec/genimg_e00*_d00.png s_genimg_d00.gif
+#convert -resize 240x -delay 20 -loop 0 dec/genimg_e00*_d01.png s_genimg_d01.gif
+#convert -resize 240x -delay 20 -loop 0 dec/genimg_e00*_d02.png s_genimg_d02.gif
+#convert -resize 240x -delay 20 -loop 0 dec/genimg_e00*_d03.png s_genimg_d03.gif
+#convert -resize 240x -delay 20 -loop 0 dec/genimg_e00*_d04.png s_genimg_d04.gif
+#convert -resize 240x -delay 20 -loop 0 dec/genimg_e00*_d05.png s_genimg_d05.gif
+#convert -resize 240x -delay 20 -loop 0 dec/genimg_e00*_d06.png s_genimg_d06.gif
+#convert -resize 240x -delay 20 -loop 0 dec/genimg_e00*_d07.png s_genimg_d07.gif
+#convert -resize 240x -delay 20 -loop 0 dec/genimg_e00*_d08.png s_genimg_d08.gif
+#convert -resize 240x -delay 20 -loop 0 dec/genimg_e00*_d09.png s_genimg_d09.gif
+
